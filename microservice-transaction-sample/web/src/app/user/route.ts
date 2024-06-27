f7030b113f616a6593a0a3fe2ab65662d67a996a
fummicc1 <fumiya.tennis1@gmail.com>
2024-06-27 17:48:55 +0900

    Create user api route

--- microservice-transaction-sample/web/src/app/user/route.ts
+++ microservice-transaction-sample/web/src/app/user/route.ts
@@ -1,3 +1,13 @@
-export const GET = async (args: { userId: number }) => {
-  await ``;
+import { NextResponse } from "next/server";
+import { $ } from "zx";
+
+export const GET = async (request: Request) => {
+  const { searchParams } = new URL(request.url);
+  const message = searchParams.get("message");
+  const serverId = searchParams.get("server_id");
+  console.log({ message, serverId });
+  const exec =
+    await $`cd ..; ./gradlew :client:run --args="GetUserFromCassandra 4"`;
+  const response = exec.stdout;
+  return NextResponse.json({ response });
 };
