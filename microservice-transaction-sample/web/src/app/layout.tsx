e693ed748ae8774e4a6133a53873fae93e8dbc2a
Gaku1031 <gaku.1031.basket@gmail.com>
2024-07-10 20:55:02 +0900

    feat: :art: 更新ボタンを設置

--- microservice-transaction-sample/web/src/app/layout.tsx
+++ microservice-transaction-sample/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,17 @@ export default function RootLayout({
         <title>投稿一覧</title>
       </Head>
       <body className="flex w-full">
-        <header className="w-full bg-white shadow-md text-black text-center py-4 text-2xl font-bold fixed top-0 z-10">
+        <header className="w-full px-10 justify-end bg-white shadow-md py-4 fixed top-0 z-10 text-black text-center text-2xl font-bold">
           Server A
+          <button className="hover:opacity-70 absolute top-4 right-10">
+            <Image
+              src="Icon/reload.svg"
+              alt="reload"
+              width={24}
+              height={24}
+              className="inline-block"
+            />
+          </button>
         </header>
         <main className="flex-1 bg-slate-100">{children}</main>
       </body>
